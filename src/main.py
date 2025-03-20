955779888bf3a2c42b61b79b10a5d94622004f1f
Matheus Gomes <matheus.gomes.m@bairesdev.com>
2025-03-20 15:05:23 -0300

    Adding code

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info("Starting application...")
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
